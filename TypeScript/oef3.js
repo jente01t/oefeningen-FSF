821241d9825c3890507ed6b548c4a8c329c163b4
jente_tav <54480755+jente01t@users.noreply.github.com>
2023-12-21 15:39:24 +0100

    update  main

---
+++ TypeScript/oef3.js
@@ -0,0 +1,6 @@
+var nummer = 1;
+var oef3 = function (nummer) {
+    nummer = "2";
+    console.log(nummer);
+};
+oef3(nummer);
---
+++ TypeScript/oef3.ts
@@ -0,0 +1,8 @@
+let nummer = 1;
+
+const oef3 = (nummer: unknown) => {
+    nummer = "2";
+    console.log(nummer);
+}
+
+oef3(nummer);
