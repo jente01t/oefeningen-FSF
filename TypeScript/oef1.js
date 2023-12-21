d8227a9f70392488379859923c16b08cc59aaa89
jente_tav <54480755+jente01t@users.noreply.github.com>
2023-12-21 15:24:20 +0100

    update main

---
+++ TypeScript/oef1.js
@@ -0,0 +1,4 @@
+var oef1 = function (naam, leeftijd) {
+    console.log(naam + " " + leeftijd);
+};
+oef1("Tom", 20);
---
+++ TypeScript/oef1.ts
@@ -0,0 +1,5 @@
+const oef1 = (naam: string, leeftijd: number) => {
+    console.log(naam + " " + leeftijd);
+}
+
+oef1("Tom", 20);
